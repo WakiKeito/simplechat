4dc9c9115b5c132b8ece09590421db07eafd9485
Waki Keito <127708830+WakiKeito@users.noreply.github.com>
2025-04-30 15:54:49 +0900

    Update bedrock-chatbot.ts

--- bin/bedrock-chatbot.ts
+++ bin/bedrock-chatbot.ts
@@ -4,17 +4,19 @@ import * as cdk from 'aws-cdk-lib';
 import { BedrockChatbotStack } from '../lib/bedrock-chatbot-stack';
 
 const app = new cdk.App();
-new BedrockChatbotStack(app, 'BedrockChatbotStack', {
+new BedrockChatbotStack(app, 'BedrockChatbotStack'
+      // ,{
   // モデルIDをオプションで指定可能
   //modelId: 'us.amazon.nova-lite-v1:0',
-  modelId: 'us.amazon.nova-micro-v1:0',
+  // modelId: 'us.amazon.nova-micro-v1:0',
   
   // 環境変数から取得したリージョンを使用、またはデフォルトとしてus-east-1を使用
-  env: { 
-    account: process.env.CDK_DEFAULT_ACCOUNT, 
-    region: process.env.CDK_DEFAULT_REGION || 'us-east-1'
-  },
-});
+//   env: { 
+//     account: process.env.CDK_DEFAULT_ACCOUNT, 
+//     region: process.env.CDK_DEFAULT_REGION || 'us-east-1'
+//   },
+// }
+  );
 
 // タグはスタックレベルで追加
 cdk.Tags.of(app).add('Project', 'BedrockChatbot');
